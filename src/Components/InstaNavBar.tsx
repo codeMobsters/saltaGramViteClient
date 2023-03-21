75a8732fdba3123f814c5480a56c559293d63d45
Rodrigo Moncorvo <romoncorvo@gmail.com>
2023-03-21 15:39:37 +0100

    Add create

--- src/Components/InstaNavBar.tsx
+++ src/Components/InstaNavBar.tsx
@@ -1,23 +1,28 @@
-import { Box } from '@mui/material'
-import React from 'react'
-import HomeIcon from '@mui/icons-material/Home'
-import SearchIcon from '@mui/icons-material/Search'
+import { Box } from "@mui/material";
+import React from "react";
+import HomeIcon from "@mui/icons-material/Home";
+import SearchIcon from "@mui/icons-material/Search";
+import AddBoxOutlinedIcon from "@mui/icons-material/AddBoxOutlined";
 import "./InstaNavBar.css";
 
 const InstaNavBar = () => {
   return (
     <Box className="instaNavBarBox">
-        SaltaGram
-        <ul>
-            <li>
-                Home <HomeIcon color='primary' onClick={() => {}}/>
-            </li>
-            <li>
-                Search <SearchIcon color='primary' onClick={() => {}}/>
-            </li>
-        </ul>
+      SaltaGram
+      <ul>
+        <li>
+          <HomeIcon color="primary" onClick={() => {}} />
+          Home
+        </li>
+        <li>
+          <SearchIcon color="primary" onClick={() => {}} /> Search
+        </li>
+        <li>
+          <AddBoxOutlinedIcon color="primary" onClick={() => {}} /> Create
+        </li>
+      </ul>
     </Box>
-  )
-}
+  );
+};
 
-export default InstaNavBar
+export default InstaNavBar;
