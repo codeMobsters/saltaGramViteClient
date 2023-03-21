315bda0be1e2934b523fe5f606288e1834698ee2
Rodrigo Moncorvo <romoncorvo@gmail.com>
2023-03-21 09:51:54 +0100

    Forgot to call the fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
@@ -18,6 +18,10 @@ function App() {
     }
   }
 
+  useEffect(() => {
+    getRecipe();
+  }, []);
+
   return (
     <div className="App">
       <div>
@@ -28,7 +32,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React test</h1>
       <div className="card">
         <button onClick={() => setCount(count => count + 1)}>
           count is {count}
